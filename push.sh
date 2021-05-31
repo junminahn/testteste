605c838417f2c9de3ea581d4420c39e686ec4415
Junmin Ahn <junminahn@users.noreply.github.com>
2021-05-31 11:53:46 -0700

    initial commit

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rm -rf .git
+git init
+git config user.name "Junmin Ahn"
+git config user.email 'junminahn@users.noreply.github.com'
+git config commit.gpgsign false
+git add .
+git commit -m "initial commit"
+git branch -M example
+git remote add origin https://junminahn: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@github.com/junminahn/testteste.git
+git push -u origin example
+
+# git remote set-url origin https://junminahn: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@github.com/junminahn/test-test.git
+# git branch -M example
+# # git push --force --quiet origin master:example
+# git push -u origin example
