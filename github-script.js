92c2e1a7618af9d13630621b0e71420400daac13
Junmin Ahn <junmin@button.is>
2021-06-06 16:16:18 -0700

    issue action

--- github-script.js
+++ github-script.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 // This module runs in GitHub Action `github-script`
 // see https://github.com/actions/github-script#run-a-separate-file-with-an-async-function
-module.exports = async ({ github, context }) => {
+module.exports = ({ github, context }) => {
   const owner = context.repo.owner;
   const repo = context.repo.repo;
 
-  console.log(JSON.stringify(github.event, null, 2))
-  console.log(JSON.stringify(context, null, 2))
+  console.log('github', JSON.stringify(github, null, 2));
+  console.log('content', JSON.stringify(context, null, 2));
 
   // await github.issues.create({
   //   owner,
