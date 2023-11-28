ff2cb1cb4d26b13bdcd8a5c301bb1a3cc8928c61
j.dev <junmin.dev@outlook.com>
2023-11-28 11:54:10 -0800

    eee

--- push.sh
+++ push.sh
@@ -14,6 +14,8 @@ git push -u origin example
 
 
 
+
+
 # git remote set-url origin https://junminahn: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@github.com/junminahn/test-test.git
 # git branch -M example
 # # git push --force --quiet origin master:example
